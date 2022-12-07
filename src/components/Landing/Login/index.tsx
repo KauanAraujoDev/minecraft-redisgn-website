2fb805fe33c5d7a75aabca5e8500fe71ea785567
KauanAraujoDev <kauanaraujo.dev@gmail.com>
2022-12-07 20:08:11 -0300

    Login Updates

--- src/components/Landing/Login/index.tsx
+++ src/components/Landing/Login/index.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import {ReactComponent as Logo} from '../../../assets/logo.svg';
 
@@ -8,6 +9,7 @@ import './style.css'
 export default function Login() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [rememberPassword, setRememberPassword] = useState(false)
 
     const emailChange = (event: any) => {
         setEmail(event.target.value)
@@ -16,10 +18,32 @@ export default function Login() {
     const passwordChange = (event: any) => {
         setPassword(event.target.value)
     }
-
+    
     const getDataToUserFromDatabase = () => {
-        window.alert(`E-MAIL: ${email}\nPASSWORD: ${password}`)
+        if (password.length > 10 && email.length > 10) {
+            window.alert(`E-mail: ${email}\nPassword: ${password}\nRemember: ${rememberPassword}`)
+        } else {
+            window.alert('Minimum number of characters is ten')
+        }
     }
+
+    const ButtonConfirmLogin = styled.div`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        position: relative;
+        width: 23.5rem;
+        height: 3rem;
+        left: 2.2rem;
+        top: 4rem;
+        
+        background: ${password.length > 10 && email.length > 10 ? 'linear-gradient(90.89deg, #22AE75 0%, #00874F 52.36%)' : 'rgba(255, 255, 255, 0.4)'};
+        box-shadow: 0rem 0rem 0rem .5rem rgba(255, 255, 255, 0.05), 0rem .125rem 0rem #FFFFFF, inset 0rem .125rem 0rem rgba(255, 255, 255, 0.4);
+        border-radius: .375rem;
+
+        color: ${password.length > 10 && email.length > 10 ? '#FFFFFF' : '#C6C6C6'};
+    `
 
     return (
         <React.Fragment>
@@ -32,18 +56,18 @@ export default function Login() {
                     <input onChange={ emailChange } type="email" placeholder='kauanaraujo.dev@gmail.com' required/>
                 </div>
                 <div className='login-input password'>
-                    <h3>Senha</h3>
-                    <input onChange={ passwordChange } type="password" placeholder='123456789' required/>
+                    <h3>Password</h3>
+                    <input onChange={ passwordChange } type="password" placeholder='your password' required/>
                 </div>
-                <div className='login-remember-me'>
+                <div className='login-remember-me' onClick={() => rememberPassword ? setRememberPassword(false) : setRememberPassword(true)}>
                     <input type="checkbox" id='rememberMeCheckBox'/>
-                    <label htmlFor="rememberMeCheckBox" className='checkboxLabel'>Lembrar minha senha</label>
+                    <label htmlFor="rememberMeCheckBox" className='checkboxLabel'>Remember my password</label>
                 </div>
-                <div className='login-button-confirm' onClick={() => { getDataToUserFromDatabase()} }>
-                    <h3>Entrar na conta</h3>
-                </div>
+                <ButtonConfirmLogin className='login-button-confirm' onClick={() => getDataToUserFromDatabase()}>
+                    <h3>Login Account</h3>
+                </ButtonConfirmLogin>
                 <div className='login-forgot-password'>
-                    <h5>Esqueceu a senha?</h5>
+                    <h5>Forgot password?</h5>
                 </div>
             </div>
         </React.Fragment>
